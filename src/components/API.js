2c9670a489270d176639130d4a1e75fc6ab27da1
opoberezh <opoberezh.2015@gmail.com>
2023-08-17 23:32:27 +0200

    coding

---
+++ src/components/API.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const API_KEY = '38119446-41822b71524f1b118d79216dc'
+
+export const getImages = async () => {
+
+}
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,75 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { nanoid } from 'nanoid'
+import { Component } from 'react';
+
+
+
+
+export class App extends Component {
+  state ={
+    query: '',
+    images: [],
+    page: 1,
+    // loading: false,
+  }
+
+changeQuery = newQuery => {
+this.setState({
+  query:`${nanoid()}/${newQuery}`,
+  images: [],
+  page: 1,
+  
+})
+}
+
+
+componentDidUpdate(pervProps, prevState){
+  const prevQuery = prevState.query;
+  const newQuery =  this.state.query;
+
+  const prevPage = prevState.page;
+  const nextPage = this.state.page;
+
+  if(prevQuery !== newQuery || prevPage !== nextPage){
+console.log(`HTTP request ${newQuery} and page ${nextPage}`)
+  }
+
+}
+
+handleLoadMore = () => {
+  if (this.state.query.trim() !== ''){
+     this.setState(prevState =>({page: prevState.page +1}))
+  }else {
+    toast("🦄 Oops! Search query is empty!");
+  }
+ 
 };
+
+
+render () {
+  return (
+    <div>
+      <form onSubmit={evt => {
+        evt.preventDefault();
+        const newQuery = evt.target.elements.query.value.trim();
+        if (newQuery === '') {
+          toast("🦄 Oops! Search query is empty!");
+          return;
+        }
+        this.changeQuery(newQuery);
+        evt.target.reset();
+      }}
+      >
+        <input type="text" name="query"/>
+        <button type="submit">Submit</button>
+      </form>
+      <div>Gallery</div>
+      <button onClick={this.handleLoadMore}>Load more</button>
+      <ToastContainer position="top-center" autoClose={2000}/>
+    </div>
+  )
+};
+}
+  
