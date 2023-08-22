0d3da8209e3544975eacdbbf634a42c2e6293290
opoberezh <opoberezh.2015@gmail.com>
2023-08-22 16:20:53 +0200

    styledBtn

--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -1,28 +1,24 @@
 import styled from 'styled-components';
 
 export const Button = styled.button`
-  margin: 10px auto;
- 
-  padding: 8px 16px;
-  border-radius: 2px;
-  background-color: #3f51b5;
-  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
-  text-align: center;
-  display: inline-block;
-  color: #fff;
-  border: 0;
-  text-decoration: none;
-  cursor: pointer;
-  font-family: inherit;
-  font-size: 18px;
-  line-height: 24px;
-  font-style: normal;
+  box-sizing: border-box;
+  width: 127px;
+  height: 45px;
+
+  margin: 10px 24px;
+  border-radius: 4px;
+  background-color: #0047AB;
+  border: 1px solid #0047AB;
+  color: #f8f8f8;
+  font-size: 19px;
   font-weight: 500;
-  min-width: 180px;
+  cursor: pointer;
   box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
-    0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
-  :hover,
-  :focus {
-    background-color: #303f9f;
-  }
+   0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
+  transition: color 300ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  &:hover {
+    background-color: #f8f8f8;
+    border: 1px solid #0047AB;
+    color:#0047AB;
 `;
